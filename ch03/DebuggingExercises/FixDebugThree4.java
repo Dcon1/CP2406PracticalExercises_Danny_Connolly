2ae675553876cfbea7458908748d577a8e9f23bb
Danny <danny.connolly@my.jcu.edu.au>
2019-09-04 19:40:03 +1000

    Fix Debugs 1 -4 Pushed for review

--- ch03/DebuggingExercises/FixDebugThree4.java
+++ ch03/DebuggingExercises/FixDebugThree4.java
@@ -1,17 +1,17 @@
 // This program calculates tutition bills as credits times rate per credit hour
-public class DebugThree4
+public class FixDebugThree4
 {
    public static void main(String args[])
    {
-      int myCredits = 13
-      int yourCredits = 17
-      double rate = 75.84
+      double myCredits = 13.00;
+      int yourCredits = 17;
+      double rate = 75.84;
       System.out.println("My tuition:");
       tuitionBill(myCredits, rate);
       System.out.println("Your tuition:");
       tuitionBill(myCredits, rate);
    }
-   public static void tuitionBill(double r)
+   private static void tuitionBill(double r, double c)
    {
       System.out.println("Total due " + (r*c));
    }
